0d14c266fbaf967d5be33697af9e0657fb7be5be
Travis Wagner <travis.wagner@umusic.com>
2023-06-02 13:33:44 -0400

    update script to use rebase

--- sync-upstream.sh
+++ sync-upstream.sh
@@ -1,49 +1,49 @@
 #!/usr/bin/env bash
 
 # check if we're on the "upstream" branch, and if not, switch to it
-upstream_branch=$(git rev-parse --abbrev-ref HEAD)
-if [ "$upstream_branch" != "upstream" ]; then
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+if [ "$current_branch" != "upstream" ]; then
     # check if the branch already exists locally
-    git branch | grep -q upstream
-    if [ $? -ne 0 ]; then
-        git checkout master
-      git checkout -b upstream
+    if ! git branch | grep -q upstream; then
+        git checkout -b upstream || echo "Failed to create and checkout upstream" && exit 1
     else
-      git checkout upstream
+        git checkout upstream || echo "Failed to checkout upstream" && exit 1
     fi
 fi
 
 # add the remote "upstream" if it doesn't exist
-git remote | grep -q upstream
-if [ $? -ne 0 ]; then
-  git remote add upstream https://github.com/rust-lang/rust.git
+if ! git remote | grep -q upstream; then
+    git remote add upstream https://github.com/rust-lang/rust.git || echo "Failed to add upstream remote" && exit 1
 fi
 
 # hard reset to the remote "upstream" and branch "master"
-git fetch upstream
-git reset --hard upstream/master
+git fetch upstream || echo "Failed to fetch upstream" && exit 1
+git reset --hard upstream/master || echo "Failed to reset to upstream/master" && exit 1
 
 # check if we're on the "current" branch, and if not, switch to it
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 if [ "$current_branch" != "current" ]; then
     # check if the branch already exists locally
-    git branch | grep -q current
-    if [ $? -ne 0 ]; then
-      git checkout master
-      git checkout -b current
+    if ! git branch | grep -q current; then
+        git checkout -b current || echo "Failed to create and checkout current" && exit 1
     else
-      git checkout current
-      git reset --hard master
+        git checkout current || echo "Failed to checkout current" && exit 1
     fi
 fi
 
-# copy the README.md file from the local master branch
-# git checkout master -- README.md CODE_OF_CONDUCT.md CONTRIBUTING.md sync-upstream.sh
-git rebase -Xtheirs upstream
+git reset --hard master || echo "Failed to reset \"current\" to local \"master\"" && exit 1
 
-# add it to git in the current branch and create a commit
-# git add --all
-# git commit -m "incorporate stable changes"
+# rebase the local "current" branch to the local "upstream" branch, favoring the "current" branch
+git rebase -Xtheirs upstream || echo "Failed to rebase \"current\" with \"upstream\"" && exit 1
 
-# git push git paid
-# git push --force origin current
+
+# make sure we are on the "current" branch
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+if [ "$current_branch" != "current" ]; then
+    echo "Not on \"current\" branch, aborting"
+    exit 1
+fi
+
+# push the changes to the remote "current" branch
+git push --force origin current || echo "Failed to push \"current\" to remote" && exit 1
+
