7f6b87b51d19c1d5c60e4e96c1c551cf465e3488
Lleyton Gray <lleyton@fyralabs.com>
2023-06-02 02:07:27 -0700

    fix: normalize test stderr

--- tests/rustdoc-ui/ice-bug-report-url.rs
+++ tests/rustdoc-ui/ice-bug-report-url.rs
@@ -4,7 +4,7 @@
 // error-pattern: we would appreciate a bug report: https://github.com/rust-lang/rust/issues/new?labels=C-bug%2C+I-ICE%2C+T-rustdoc&template=ice.md
 
 // normalize-stderr-test "note: compiler flags.*\n\n" -> ""
-// normalize-stderr-test "note: rustc.*running on.*" -> "note: rustc {version} running on {platform}"
+// normalize-stderr-test "note: crabc.*running on.*" -> "note: crabc {version} running on {platform}"
 // normalize-stderr-test "thread.*panicked at .*, compiler.*" -> "thread panicked at 'aborting due to `-Z treat-err-as-bug`'"
 // normalize-stderr-test " +\d{1,}: .*\n" -> ""
 // normalize-stderr-test " + at .*\n" -> ""
