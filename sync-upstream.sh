04db417efabaead8d713c07260cd33e6b7262e94
Travis Wagner <travis.wagner@umusic.com>
2023-06-02 13:18:32 -0400

    update to use rebase

--- sync-upstream.sh
+++ sync-upstream.sh
@@ -1,14 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# check if we're on the "current" branch, and if not, switch to it
-current_branch=$(git rev-parse --abbrev-ref HEAD)
-if [ "$current_branch" != "current" ]; then
+# check if we're on the "upstream" branch, and if not, switch to it
+upstream_branch=$(git rev-parse --abbrev-ref HEAD)
+if [ "$upstream_branch" != "upstream" ]; then
     # check if the branch already exists locally
-    git branch | grep -q current
+    git branch | grep -q upstream
     if [ $? -ne 0 ]; then
-      git checkout -b current
+        git checkout master
+      git checkout -b upstream
     else
-      git checkout current
+      git checkout upstream
     fi
 fi
 
@@ -22,12 +23,27 @@ fi
 git fetch upstream
 git reset --hard upstream/master
 
+# check if we're on the "current" branch, and if not, switch to it
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+if [ "$current_branch" != "current" ]; then
+    # check if the branch already exists locally
+    git branch | grep -q current
+    if [ $? -ne 0 ]; then
+      git checkout master
+      git checkout -b current
+    else
+      git checkout current
+      git reset --hard master
+    fi
+fi
+
 # copy the README.md file from the local master branch
-git checkout master -- README.md CODE_OF_CONDUCT.md CONTRIBUTING.md sync-upstream.sh
+# git checkout master -- README.md CODE_OF_CONDUCT.md CONTRIBUTING.md sync-upstream.sh
+git rebase -Xtheirs upstream
 
 # add it to git in the current branch and create a commit
-git add --all
-git commit -m "incorporate stable changes"
+# git add --all
+# git commit -m "incorporate stable changes"
 
 # git push git paid
-git push --force origin current
+# git push --force origin current
